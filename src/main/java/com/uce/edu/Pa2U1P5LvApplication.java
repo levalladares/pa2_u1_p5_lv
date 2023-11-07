a897d7fd62fa476767703a0acb1a2f2bf7dc4229
Luis <levalladares@uce.edu.ec>
2023-11-07 18:57:32 -0500

    mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5LvApplication.java
@@ -0,0 +1,23 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5LvApplication implements CommandLineRunner
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5LvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		System.out.println("Soy Luis Valladares");
+	
+	}
+
+}
